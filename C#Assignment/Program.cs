e9ac12aab87d32babb30dd0a1cf286e92ebd274b
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-11-05 10:54:05 -0400

    Welcome Screen

---
+++ C#Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace C_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, We're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
