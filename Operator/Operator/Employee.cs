882c807b4fc056b59923b353c95cbd16514ddb41
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-12-13 16:11:19 -0400

    Operator

---
+++ Operator/Operator/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Constructor to initialize Employee
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Overload "==" and "!=" operators
+        public static bool operator ==(Employee emp1, Employee emp2) => emp1?.Id == emp2?.Id;
+        public static bool operator !=(Employee emp1, Employee emp2) => !(emp1 == emp2);
+
+        public override string ToString() => $"{FirstName} {LastName} (ID: {Id})";
+    }
+}
---
+++ Operator/Operator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(101, "John", "Doe");
+            Employee emp2 = new Employee(102, "Jane", "Smith");
+
+            Console.WriteLine(emp1);
+            Console.WriteLine(emp2);
+
+            // Compare employees using the overloaded operators
+            Console.WriteLine(emp1 == emp2 ? "Same employee" : "Different employees");
+            Console.WriteLine(emp1 != emp2 ? "Different employees" : "Same employee");
+
+            Console.ReadLine();
+        }
+    }
+}
