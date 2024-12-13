4cb2737326fb15e22a1002cfc226a9a65b806591
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-12-13 16:00:03 -0400

    Abstract assignment

---
+++ abstract/abstract/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Create an Employee object.
+        Employee employee = new Employee("Sample", "Student");
+
+        // Call the SayName() method on the Employee object.
+        employee.SayName();
+
+        // Prevent console from closing immediately.
+        Console.ReadLine();
+    }
+}
