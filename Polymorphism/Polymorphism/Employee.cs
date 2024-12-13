daa215e39f79f07c70f9a55a96cc6ff60ac6a3c0
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-12-13 16:05:32 -0400

    Polymorphism

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Employee : IQuittable
+{
+    // Properties for the Employee class
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Constructor to initialize Employee's first and last name.
+    public Employee(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    // Implementing the Quit() method from IQuittable interface
+    public void Quit()
+    {
+        // Display a message when the employee quits
+        Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+    }
+}
