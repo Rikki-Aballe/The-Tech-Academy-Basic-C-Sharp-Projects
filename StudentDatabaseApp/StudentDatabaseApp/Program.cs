4c54be73086a3bc9da37c056fae4a15170b0d107
Rikki-Aballe <rikkiaballe02@gmail.com>
2024-12-17 16:55:25 -0400

    Assignment

---
+++ StudentDatabaseApp/StudentDatabaseApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class StudentContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new StudentContext())
+        {
+            var student = new Student
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully.");
+        }
+    }
+}
